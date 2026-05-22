75f09b7484af5014fa4b697a4704e35cd0d609c0
Ilyes <ilyess77680@gmail.com>
2026-05-22 08:30:42 +0000

    Initial DevOps monitoring platform

---
+++ .terraform.lock.hcl
@@ -0,0 +1,21 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/kreuzwerker/docker" {
+  version = "4.4.0"
+  hashes = [
+    "h1:EJxOr2QM2pYk0vPLXvnOaxolzVOMwVl8+nRDLXULVJw=",
+    "zh:0ae16c1fd4a2d6d8b54791e2fbca20381fb9fc29809ee59d4f422ac636c9b659",
+    "zh:0f92b1d5296d80b90daff0909b8aeb9ed5c3db1436c74016a8ecbf6a57fafe1b",
+    "zh:29fb91dd3e3b6de5ce36f272e527b6efcdbcd83c8eb0679402a1985ab1a3d7fd",
+    "zh:4068cc8ec1a9d964d782798903a9632fdd1c92980b11df7f5800a98a312e4a8b",
+    "zh:5a094282a227eb3f5c9bd34a487a514de4544bf2a735a546dc53bd501addca1b",
+    "zh:705347ab6e39a4a926d9d6c6789a6c23cc09e103bec72209fe9267533c92e200",
+    "zh:74216104006de3252d13790c101415d4ff8f09bbd94055a58e8ad6179fea3b55",
+    "zh:86966e73330cf76c92305fb71ce3d6dd560f66d0ea307a94b34f06dfb1075694",
+    "zh:938bb9e908b484abf4a800faa32476f41a3c20d531a186b65a76f924fda7b9d6",
+    "zh:a9a79377d7635f9cb1972680d005aca00dccf5d484d8486697eacbeb856756f2",
+    "zh:cfc1645a65cf5b66d8d9d62666cd069edcee1a8e16df4f78e57b7ffe71c43778",
+    "zh:f38022ff9a675864ee8f8e53c5121731dd4964703c2f7c35bc702a09c4a727a3",
+  ]
+}
---
+++ main.tf
@@ -0,0 +1,210 @@
+terraform {
+  required_providers {
+    docker = {
+      source = "kreuzwerker/docker"
+    }
+  }
+}
+
+provider "docker" {}
+
+resource "docker_network" "monitoring" {
+  name = "monitoring-network"
+}
+
+########################
+# NGINX
+########################
+
+resource "docker_image" "nginx" {
+  name = "nginx:latest"
+}
+
+resource "docker_container" "web" {
+  name  = "web"
+  image = docker_image.nginx.image_id
+
+  networks_advanced {
+    name = docker_network.monitoring.name
+  }
+
+  ports {
+    internal = 80
+    external = 8080
+  }
+}
+
+########################
+# NODE EXPORTER
+########################
+
+resource "docker_image" "node_exporter" {
+  name = "prom/node-exporter"
+}
+
+resource "docker_container" "node_exporter" {
+  name  = "node-exporter"
+  image = docker_image.node_exporter.image_id
+
+  networks_advanced {
+    name = docker_network.monitoring.name
+  }
+
+  ports {
+    internal = 9100
+    external = 9100
+  }
+}
+
+########################
+# PROMETHEUS
+########################
+
+resource "docker_image" "prometheus" {
+  name = "prom/prometheus"
+}
+
+resource "docker_container" "prometheus" {
+  name  = "prometheus"
+  image = docker_image.prometheus.image_id
+
+  networks_advanced {
+    name = docker_network.monitoring.name
+  }
+
+  volumes {
+    host_path      = "/root/devops-project/terraform/prometheus"
+    container_path = "/etc/prometheus"
+  }
+
+  ports {
+    internal = 9090
+    external = 9090
+  }
+}
+
+########################
+# GRAFANA
+########################
+
+resource "docker_image" "grafana" {
+  name = "grafana/grafana"
+}
+
+resource "docker_container" "grafana" {
+  name  = "grafana"
+  image = docker_image.grafana.image_id
+
+  networks_advanced {
+    name = docker_network.monitoring.name
+  }
+
+  ports {
+    internal = 3000
+    external = 3000
+  }
+}
+
+########################
+# SURICATA
+########################
+
+resource "docker_image" "suricata" {
+  name = "jasonish/suricata:latest"
+}
+
+resource "docker_container" "suricata" {
+
+  name  = "suricata"
+
+  image = docker_image.suricata.image_id
+
+  command = [
+    "-i",
+    "eth0"
+  ]
+
+  networks_advanced {
+      name = docker_network.monitoring.name
+  }
+
+  capabilities {
+      add = ["NET_ADMIN","NET_RAW"]
+  }
+
+  volumes {
+      host_path="/root/devops-project/terraform/suricata/logs"
+      container_path="/var/log/suricata"
+  }
+
+  volumes {
+   host_path="/root/devops-project/terraform/suricata/rules"
+   container_path="/var/lib/suricata/rules"
+  }
+
+}
+
+########################
+# LOKI
+########################
+
+resource "docker_image" "loki" {
+  name="grafana/loki"
+}
+
+resource "docker_container" "loki" {
+
+  name="loki"
+
+  image=docker_image.loki.image_id
+
+  networks_advanced {
+      name=docker_network.monitoring.name
+  }
+
+  ports {
+      internal=3100
+      external=3100
+  }
+
+}
+
+########################
+# PROMTAIL
+########################
+
+resource "docker_image" "promtail" {
+ name="grafana/promtail"
+}
+
+resource "docker_container" "promtail" {
+
+ name="promtail"
+
+ image=docker_image.promtail.image_id
+
+ networks_advanced {
+     name=docker_network.monitoring.name
+ }
+
+ volumes {
+
+   host_path="/root/devops-project/terraform/promtail"
+   container_path="/etc/promtail"
+
+ }
+
+ volumes {
+
+   host_path="/root/devops-project/terraform/suricata/logs"
+   container_path="/var/log/suricata"
+
+ }
+
+ command=[
+
+ "-config.file=/etc/promtail/promtail-config.yml"
+
+ ]
+
+}
